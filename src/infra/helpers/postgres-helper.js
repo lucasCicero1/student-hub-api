dfa71338a6e09eb21d9e8b4c19e1f39980b2dc1a
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-14 19:55:34 -0300

    feat: ensure PostgresHelper connect and disconnect from database

---
+++ src/infra/helpers/postgres-helper.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+class PostgresHelper {
+  #configs;
+
+  client;
+
+  constructor(configs) {
+    this.#configs = configs;
+  }
+
+  async connect() {
+    if (!this.client) {
+      this.client = new Client(this.#configs);
+      await this.client.connect();
+    }
+
+    return this.client;
+  }
+
+  async disconnect() {
+    await this.client.end();
+    this.client = null;
+  }
+}
+
+export { PostgresHelper };
