9c471ce3767f493abb7e4a5575836352942f1596
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-14 19:57:17 -0300

    build: create development.students table and insert some data on initdb.sql file

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -9,3 +9,17 @@ create table test.students (
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
+
+CREATE SCHEMA IF NOT EXISTS development;
+
+create table development.students (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    ra SERIAL UNIQUE NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+
+INSERT INTO development.students (name, email, cpf) VALUES ('joão', 'joao@mail.com', '12345678945'), ('maria', 'maria@mail.com', '56345678342')
