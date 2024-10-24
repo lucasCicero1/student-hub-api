b97cfe35a31141d95549e5d14dd707786f871c9c
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-24 14:17:30 -0300

    test: ensure EmailValidatorAdapter calls the validator with correct values

--- tests/infra/validators/email-validator-adapter.spec.js
+++ tests/infra/validators/email-validator-adapter.spec.js
@@ -24,4 +24,11 @@ describe("EmailValidator Adapter", () => {
     const isValid = sut.isValid("valid_email@mail.com");
     expect(isValid).toBe(true);
   });
+
+  test("Should call validator with correct email", () => {
+    const sut = makeSut();
+    const validatorSpy = jest.spyOn(validator, "isEmail");
+    sut.isValid("valid_email@mail.com");
+    expect(validatorSpy).toHaveBeenCalledWith("valid_email@mail.com");
+  });
 });
