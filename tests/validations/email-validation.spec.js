166f868e3ed02f938d43f915f4adadeb33e40d6b
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-24 14:36:15 -0300

    test: ensure EmailValidation calls the email validator with correct values

---
+++ tests/validations/email-validation.spec.js
@@ -0,0 +1,28 @@
+import { EmailValidation } from "../../src/validations";
+
+const makeEmailValidator = () => {
+  class EmailValidatorStub {
+    isValid() {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+const makeSut = () => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new EmailValidation("email", emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub,
+  };
+};
+
+describe("Email Validation", () => {
+  test("Should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+    sut.validate({ email: "any_email" });
+    expect(isValidSpy).toHaveBeenCalledWith("any_email");
+  });
+});
