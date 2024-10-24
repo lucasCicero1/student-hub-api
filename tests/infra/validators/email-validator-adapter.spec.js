209b14bc861002c875609fbcbdbdd82686670a1d
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-24 14:10:46 -0300

    test: ensure EmailValidatorAdapter returns true if validator also returns true

--- tests/infra/validators/email-validator-adapter.spec.js
+++ tests/infra/validators/email-validator-adapter.spec.js
@@ -18,4 +18,10 @@ describe("EmailValidator Adapter", () => {
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
+
+  test("Should return true if validator returns true", () => {
+    const sut = makeSut();
+    const isValid = sut.isValid("valid_email@mail.com");
+    expect(isValid).toBe(true);
+  });
 });
