4ac48e94c3f7b6a3bca68b51559f275666ca060a
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-24 13:57:21 -0300

    feat: create invalid param and missing param error files

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,3 +1,5 @@
 import ServerError from "./server-error";
+import MissingParamError from "./missing-param-error";
+import InvalidParamError from "./invalid-param-error";
 
-export { ServerError };
+export { ServerError, MissingParamError, InvalidParamError };
