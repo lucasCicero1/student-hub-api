8de83b76461614ca350d3b750f026ce6df6dd974
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-24 20:58:38 -0300

    test: ensure CreateStudentController calls createStudentUseCase with correct values

---
+++ tests/presentation/controllers/create-student-controller.spec.js
@@ -0,0 +1,48 @@
+import { CreateStudentController } from "../../../src/presentation/controllers";
+
+const makeCreateStudentUseCase = () => {
+  class CreateStudentUseCaseStub {
+    async create() {
+      await Promise.resolve();
+    }
+  }
+  return new CreateStudentUseCaseStub();
+};
+
+const makeValidations = () => {
+  class ValidationStub {
+    validate() {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = () => {
+  const createUseCaseStub = makeCreateStudentUseCase();
+  const validationsStub = makeValidations();
+  const sut = new CreateStudentController({
+    createStudentUseCase: createUseCaseStub,
+    validation: validationsStub,
+  });
+  return {
+    sut,
+    createUseCaseStub,
+    validationsStub,
+  };
+};
+
+const fakeQuery = () => ({
+  name: "fake-name",
+  email: "fake-email@mail.com",
+  cpf: "84567329460",
+});
+
+describe("List Students Controller", () => {
+  test("Should call createStudentUseCase with correct values", async () => {
+    const { sut, createUseCaseStub } = makeSut();
+    const createUseCaseSpy = jest.spyOn(createUseCaseStub, "create");
+    await sut.handle({ body: fakeQuery() });
+    expect(createUseCaseSpy).toHaveBeenCalledWith(fakeQuery());
+  });
+});
