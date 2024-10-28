be8fbbe4dfea1d4fccc4a696e0ee7e90ab505d2c
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-28 19:29:34 -0300

    feat: ensure UpdateStudentController calls validation with correct values

--- src/presentation/controllers/update-student-controller.js
+++ src/presentation/controllers/update-student-controller.js
@@ -3,12 +3,16 @@ import HttpResponse from "../helpers/http-response";
 export default class UpdateStudentController {
   #updateStudentUseCase;
 
-  constructor({ updateStudentUseCase }) {
+  #validation;
+
+  constructor({ updateStudentUseCase, validation }) {
     this.#updateStudentUseCase = updateStudentUseCase;
+    this.#validation = validation;
   }
 
   async handle(httpRequest) {
     try {
+      this.#validation.validate(httpRequest.body);
       return await this.#updateStudentUseCase.update(httpRequest.body);
     } catch (error) {
       return this.#switchResponse(error);
