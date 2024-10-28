56556d18a7d895e01bff2b8f64760c10b8e3b0d6
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-28 19:25:22 -0300

    test: ensure UpdateStudentController calls updateStudentUseCase with correct values

---
+++ tests/presentation/controllers/update-student-controller.spec.js
@@ -0,0 +1,46 @@
+import { UpdateStudentController } from "../../../src/presentation/controllers";
+
+const makeUpdateStudentUseCase = () => {
+  class UpdateStudentUseCaseStub {
+    async update() {
+      await Promise.resolve();
+    }
+  }
+  return new UpdateStudentUseCaseStub();
+};
+
+const makeValidations = () => {
+  class ValidationStub {
+    validate() {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = () => {
+  const updateUseCaseStub = makeUpdateStudentUseCase();
+  const validationsStub = makeValidations();
+  const sut = new UpdateStudentController({
+    updateStudentUseCase: updateUseCaseStub,
+    validation: validationsStub,
+  });
+  return {
+    sut,
+    updateUseCaseStub,
+    validationsStub,
+  };
+};
+
+const fakeQuery = () => ({
+  cpf: "84567329460",
+});
+
+describe("Update Student Controller", () => {
+  test("Should call updateStudentUseCase with correct values", async () => {
+    const { sut, updateUseCaseStub } = makeSut();
+    const updateUseCaseSpy = jest.spyOn(updateUseCaseStub, "update");
+    await sut.handle({ body: fakeQuery() });
+    expect(updateUseCaseSpy).toHaveBeenCalledWith(fakeQuery());
+  });
+});
