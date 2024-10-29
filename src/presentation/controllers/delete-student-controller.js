a913171359fce14ec60f99a30d0a444ea87c2ad0
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 15:15:04 -0300

    feat: ensure DeleteStudentController calls validation with correct values

--- src/presentation/controllers/delete-student-controller.js
+++ src/presentation/controllers/delete-student-controller.js
@@ -1,11 +1,15 @@
 export default class DeleteStudentController {
   #deleteStudentUseCase;
 
-  constructor({ deleteStudentUseCase }) {
+  #validation;
+
+  constructor({ deleteStudentUseCase, validation }) {
     this.#deleteStudentUseCase = deleteStudentUseCase;
+    this.#validation = validation;
   }
 
   async handle(httpRequest) {
+    this.#validation.validate(httpRequest.body);
     await this.#deleteStudentUseCase.delete(httpRequest.body);
   }
 }
