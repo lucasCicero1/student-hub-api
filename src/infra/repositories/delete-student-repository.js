860363e944a112d0e8e3b91c155346632bd2d8b3
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 14:56:29 -0300

    feat: ensure DeleteStudentRepository deletes the student

---
+++ src/infra/repositories/delete-student-repository.js
@@ -0,0 +1,36 @@
+export default class DeleteStudentRepository {
+  #postgresHelper;
+
+  constructor(postgresHelper) {
+    this.#postgresHelper = postgresHelper;
+  }
+
+  async delete({ cpf }) {
+    let client;
+    try {
+      client = await this.#postgresHelper.connect();
+      await client.query("BEGIN;");
+      await client.query(this.sql, [cpf]);
+      await client.query("COMMIT;");
+      return true;
+    } catch (error) {
+      if (client) {
+        await client.query("ROLLBACK;");
+      }
+      throw error;
+    } finally {
+      if (client) {
+        await this.#postgresHelper.disconnect();
+      }
+    }
+  }
+
+  get sql() {
+    return `
+      DELETE FROM
+        my_schema.students
+      WHERE
+        cpf = $1
+    `;
+  }
+}
--- src/infra/repositories/index.js
+++ src/infra/repositories/index.js
@@ -1,9 +1,11 @@
 import CreateStudentRepository from "./create-student-repository";
 import ListStudentsRepository from "./list-students-repository";
 import UpdateStudentRepository from "./update-student-repository";
+import DeleteStudentRepository from "./delete-student-repository";
 
 export {
   CreateStudentRepository,
   ListStudentsRepository,
   UpdateStudentRepository,
+  DeleteStudentRepository,
 };
