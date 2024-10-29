f18e0d98cc2ebb1cad896fff00a009ec6c0ba173
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-10-29 15:23:22 -0300

    test: ensure DeleteStudentController returns 500 if deleteStudentUseCase throws

--- tests/presentation/controllers/delete-student-controller.spec.js
+++ tests/presentation/controllers/delete-student-controller.spec.js
@@ -47,9 +47,9 @@ describe("Delete Student Controller", () => {
 
   test("Should call validation with correct values", async () => {
     const { sut, validationsStub } = makeSut();
-    const updateUseCaseSpy = jest.spyOn(validationsStub, "validate");
+    const validationSpy = jest.spyOn(validationsStub, "validate");
     await sut.handle({ body: fakeQuery() });
-    expect(updateUseCaseSpy).toHaveBeenCalledWith(fakeQuery());
+    expect(validationSpy).toHaveBeenCalledWith(fakeQuery());
   });
 
   test("Should return badRequest if validation returns an error", async () => {
@@ -65,5 +65,14 @@ describe("Delete Student Controller", () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle({});
     expect(httpResponse).toEqual(HttpResponse.noContent());
+  });
+
+  test("Should return 500 if deleteStudentUseCase throws", async () => {
+    const { sut, deleteUseCaseStub } = makeSut();
+    jest
+      .spyOn(deleteUseCaseStub, "delete")
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(HttpResponse.serverError());
   });
 });
