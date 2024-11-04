2536d4b7330e2ac8fd3e804544a1d4a0af956599
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-11-04 17:17:15 -0300

    refactor: improves list students test

--- tests/infra/repositories/list-students-repository.spec.js
+++ tests/infra/repositories/list-students-repository.spec.js
@@ -1,71 +1,46 @@
-import { PostgresHelper } from "../../../src/infra/helpers/postgres-helper";
-import { Envs } from "../../../src/main/config/envs";
 import { ListStudentsRepository } from "../../../src/infra/repositories";
 
-const fakeQuery = () => ({
-  name: "fake-name",
-  email: "fake-email@mail.com",
-  cpf: "84567329460",
-});
+const makePostgresHelperMock = () => {
+  class PostgresHelperMock {
+    connect = jest.fn();
+
+    disconnect = jest.fn();
+
+    client = {
+      query: jest.fn(),
+    };
+  }
+  return new PostgresHelperMock();
+};
+
+const makeSut = () => {
+  const mockPostgresHelper = makePostgresHelperMock();
+  const sut = new ListStudentsRepository(mockPostgresHelper);
+  mockPostgresHelper.connect.mockResolvedValue(mockPostgresHelper.client);
+  return {
+    sut,
+    mockPostgresHelper,
+  };
+};
 
 describe("List Students Repository", () => {
-  const postgresHelper = new PostgresHelper(Envs.POSTGRES);
-  const makeSut = () => new ListStudentsRepository(postgresHelper);
-  let client;
-  const sql = "SELECT name, email, ra, cpf FROM fake_students";
+  const mockData = [
+    {
+      name: "test",
+      email: "test@mail.com",
+      ra: "12345",
+      cpf: "11122233344",
+    },
+  ];
 
-  beforeEach(async () => {
-    client = await postgresHelper.connect();
-    await client.query(
-      "CREATE TEMPORARY TABLE fake_students (LIKE my_schema.students INCLUDING ALL)",
-    );
-  });
-
-  afterEach(async () => {
-    client = await postgresHelper.connect();
-    await client.query("DROP TABLE IF EXISTS pg_temp.fake_students");
-  });
-
-  afterAll(async () => {
-    await postgresHelper.disconnect();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   test("Should be able to list students", async () => {
-    const { name, email, cpf } = fakeQuery();
-    await client.query(
-      "INSERT INTO fake_students (name, email, cpf) VALUES ($1, $2, $3)",
-      [name, email, cpf],
-    );
-    const sut = makeSut();
-    jest.spyOn(sut, "sql", "get").mockReturnValue(sql);
-    const [students] = await sut.listStudents();
-    expect(students).toBeTruthy();
-    expect(students.name).toBe("fake-name");
-    expect(students.email).toBe("fake-email@mail.com");
-    expect(students.ra).toBeTruthy();
-    expect(students.cpf).toBe("84567329460");
-  });
-
-  test("Should return empty array if there is no data in the database", async () => {
-    const sut = makeSut();
-    jest.spyOn(sut, "sql", "get").mockReturnValue(sql);
+    const { sut, mockPostgresHelper } = makeSut();
+    mockPostgresHelper.client.query.mockResolvedValue({ rows: mockData });
     const students = await sut.listStudents();
-    expect(students).toHaveLength(0);
-  });
-
-  test("Should be able to list a student by cpf", async () => {
-    await client.query(
-      "INSERT INTO fake_students (name, email, cpf) VALUES ('fake-name', 'fake-email@mail.com', '12345678945'), ('other-name', 'other-name@mail.com', '56345678342')",
-    );
-    const sqlByCpf =
-      "SELECT name, email, ra, cpf FROM fake_students WHERE cpf = $1";
-    const sut = makeSut();
-    jest.spyOn(sut, "sqlWithCpf", "get").mockReturnValue(sqlByCpf);
-    const [students] = await sut.listStudentByCpf({ cpf: "56345678342" });
-    expect(students).toBeTruthy();
-    expect(students.name).toBe("other-name");
-    expect(students.email).toBe("other-name@mail.com");
-    expect(students.ra).toBeTruthy();
-    expect(students.cpf).toBe("56345678342");
+    expect(students).toEqual(mockData);
   });
 });
