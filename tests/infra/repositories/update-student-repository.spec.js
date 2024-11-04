7927d62cc4008d5122b2b0c5ab588ddcf14d94e0
Lucas Cícero <lucas.cicerofs@outlook.com>
2024-11-04 19:23:04 -0300

    test: ensure UpdateStudentRepository calls PostgresHelper rollback query if any error occurs

--- tests/infra/repositories/update-student-repository.spec.js
+++ tests/infra/repositories/update-student-repository.spec.js
@@ -77,6 +77,15 @@ describe("Update Student Repository", () => {
     expect(mockPostgresHelper.client.query).toHaveBeenCalledWith("COMMIT;");
   });
 
+  test("Should call rollback if any error occurs", async () => {
+    const { sut, mockPostgresHelper } = makeSut();
+    mockPostgresHelper.client.query
+      .mockImplementationOnce()
+      .mockImplementationOnce(() => Promise.reject(new Error()));
+    await expect(sut.update(fakeQuery())).rejects.toThrow();
+    expect(mockPostgresHelper.client.query).toHaveBeenCalledWith("ROLLBACK;");
+  });
+
   test("Should call postgresHelper disconnect", async () => {
     const { sut, mockPostgresHelper } = makeSut();
     const postgresHelperSpy = jest.spyOn(mockPostgresHelper, "disconnect");
