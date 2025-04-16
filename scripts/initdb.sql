ba0615bb7fbb18385b52971954866f3179949984
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-16 18:50:01 -0300

    chore: add new create students query on initdb.sql

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -49,3 +49,28 @@ INSERT INTO my_schema.students (name, email, cpf) VALUES ('Henrique Santos', 'he
 -- );
 
 -- INSERT INTO development.students (name, email, cpf) VALUES ('joão', 'joao@mail.com', '12345678945'), ('maria', 'maria@mail.com', '56345678342')
+
+CREATE SCHEMA IF NOT EXISTS my_schema;
+
+create table if not exists my_schema.students (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    ra SERIAL UNIQUE NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    status VARCHAR(15) NOT NULL,
+    avatar CHARACTER VARYING(64) varying(64),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+INSERT INTO my_schema.students
+    (id, name, email, ra, cpf, status, avatar)
+VALUES
+(1, 'Izabella Walker', 'izabela.walker@outlook.com', 1, '91381231412', 'active', 'https://i.pravatar.cc/150?u=a042581f4e29026704d'),
+(2, 'Rayan Burnett', 'rayan.b@yahoo.com.br', 2, '85712945123', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
+(3, 'Tony Bernal', 'tony-bernal@hotmail.com', 3, '85712311234', 'vacation', 'https://i.pravatar.cc/150?u=a048581f4e29026701d'),
+(4, 'Chanel Cherry', 'chanel-cherry@gmail.com', 4, '85746375643', 'active', 'https://i.pravatar.cc/150?u=a092581d4ef9026700d'),
+(5, 'Alberto Gilmore', 'alberto-g@gmail.com', 5, '94756218742', 'vacation', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
+(6, 'Natasha Arellano', 'natasha.arellano@outlook.com', 6, '97537590123', 'active', 'https://i.pravatar.cc/150?u=a04258114e29026702d'),
+(7, 'Elina Lowe', 'elina.lowe@hotmail.com', 7, '86495899124', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026704d')
