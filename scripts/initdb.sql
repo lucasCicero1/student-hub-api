7acadea9f9ea4565aa759df33d7de8a230265f6d
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-16 22:39:35 -0300

    chore: change insert values on initdb.sql

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -59,18 +59,18 @@ create table if not exists my_schema.students (
     ra SERIAL UNIQUE NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     status VARCHAR(15) NOT NULL,
-    avatar CHARACTER VARYING(64) varying(64),
+    avatar CHARACTER VARYING(64),
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 INSERT INTO my_schema.students
-    (id, name, email, ra, cpf, status, avatar)
+    (name, email, cpf, status, avatar)
 VALUES
-(1, 'Izabella Walker', 'izabela.walker@outlook.com', 1, '91381231412', 'active', 'https://i.pravatar.cc/150?u=a042581f4e29026704d'),
-(2, 'Rayan Burnett', 'rayan.b@yahoo.com.br', 2, '85712945123', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
-(3, 'Tony Bernal', 'tony-bernal@hotmail.com', 3, '85712311234', 'vacation', 'https://i.pravatar.cc/150?u=a048581f4e29026701d'),
-(4, 'Chanel Cherry', 'chanel-cherry@gmail.com', 4, '85746375643', 'active', 'https://i.pravatar.cc/150?u=a092581d4ef9026700d'),
-(5, 'Alberto Gilmore', 'alberto-g@gmail.com', 5, '94756218742', 'vacation', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
-(6, 'Natasha Arellano', 'natasha.arellano@outlook.com', 6, '97537590123', 'active', 'https://i.pravatar.cc/150?u=a04258114e29026702d'),
-(7, 'Elina Lowe', 'elina.lowe@hotmail.com', 7, '86495899124', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026704d')
+('Izabella Walker', 'izabela.walker@outlook.com', '91381231412', 'active', 'https://i.pravatar.cc/150?u=a042581f4e29026704d'),
+('Rayan Burnett', 'rayan.b@yahoo.com.br', '85712945123', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
+('Tony Bernal', 'tony-bernal@hotmail.com', '85712311234', 'vacation', 'https://i.pravatar.cc/150?u=a048581f4e29026701d'),
+('Chanel Cherry', 'chanel-cherry@gmail.com', '85746375643', 'active', 'https://i.pravatar.cc/150?u=a092581d4ef9026700d'),
+('Alberto Gilmore', 'alberto-g@gmail.com', '94756218742', 'vacation', 'https://i.pravatar.cc/150?u=a042581f4e29026024d'),
+('Natasha Arellano', 'natasha.arellano@outlook.com', '97537590123', 'active', 'https://i.pravatar.cc/150?u=a04258114e29026702d'),
+('Elina Lowe', 'elina.lowe@hotmail.com', '86495899124', 'paused', 'https://i.pravatar.cc/150?u=a042581f4e29026704d')
