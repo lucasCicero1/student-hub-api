466e88af55b6aee3f087542f6d59c569e9a97a60
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-16 22:38:28 -0300

    chore: setup morgan middleware

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,9 @@
-import { cors, jsonParse, contentType } from "../middlewares";
+import { cors, jsonParse, contentType, morgan } from "../middlewares";
 
 export default (app) => {
   app.disable("x-powered-by");
   app.use(cors);
   app.use(jsonParse);
   app.use(contentType);
+  app.use(morgan);
 };
--- src/main/middlewares/index.js
+++ src/main/middlewares/index.js
@@ -1,5 +1,6 @@
 import cors from "./cors";
 import jsonParse from "./json-parse";
 import contentType from "./content-type";
+import morgan from "./morgan";
 
-export { cors, jsonParse, contentType };
+export { cors, jsonParse, contentType, morgan };
